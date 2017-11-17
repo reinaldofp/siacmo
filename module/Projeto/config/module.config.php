0121df92fa339085fbcfc6ab6c723a9c19bd4dcb
Reinaldo Pereira <reinaldofp@gmail.com>
2017-11-17 15:36:38 -0200

    Commit inicial - Esqueleto do SIACMO, com os módulos: Disciplina, Perfil, Produto, Projeto, Sala, Secretaria, Usuário, Venda e Integrar além do Aplication

---
+++ module/Projeto/config/module.config.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Projeto;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'projeto' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route'    => '/projeto',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+            'projeto' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/projeto[/:action]',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'projeto/index/index' => __DIR__ . '/../view/projeto/index/index.phtml',
+
+            // Páginas padrã para Layout e Error
+            'layout/layout'           => __DIR__ . '/../../Application/view/layout/layout.phtml',
+            'error/404'               => __DIR__ . '/../../Application/view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../../Application/view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+           __DIR__ . '/../view',
+        ],
+    ],
+];
